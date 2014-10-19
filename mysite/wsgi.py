31f4342fac13b0d00acf12299cd56ab022fa4371
yangtianhang <yangtianhang@ricebook.com>
2014-10-19 19:15:40 +0800

    up

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lltt project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lltt.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
