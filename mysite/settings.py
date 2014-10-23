314157d765b34be43ed2e2fafedf0b17e01b93ed
yangtianhang <yangtianhang@ricebook.com>
2014-10-23 21:08:13 +0800

    add deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '8$r!nfta#d0&0tu+4el7)m&54ex!0%!sur6zl+@6vzu7lri774'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
