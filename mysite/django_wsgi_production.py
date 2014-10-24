683d88d5e17f1b5415cfdd6cb82a8cb9c7621efb
yangtianhang <tianhang.yang@gmail.com>
2014-10-24 16:08:16 +0800

    add pro wsgi

---
+++ mysite/django_wsgi_production.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-  
+
+__author__ = 'yang'
+import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings_pro")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
